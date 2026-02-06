aceeb9f19c5e24efba9349897cdefc75437e5fa0
nevzatcirak <nevzatcirak17@hotmail.com>
2026-02-06 14:42:41 +0100

    Initial step is done!

--- internal/data/repository/store.go
+++ internal/data/repository/store.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/go-jose/go-jose/v3"
 	"github.com/nevzatcirak/shyntr/internal/data/models"
+	"github.com/nevzatcirak/shyntr/pkg/crypto"
 	"github.com/ory/fosite"
 	"gorm.io/gorm"
 )
@@ -18,12 +20,10 @@ func NewSQLStore(db *gorm.DB) *SQLStore {
 	return &SQLStore{DB: db}
 }
 
-// --- ClientManager ---
+// --- ClientManager Implementation ---
 
 func (s *SQLStore) GetClient(ctx context.Context, id string) (fosite.Client, error) {
 	var clientModel models.OAuth2Client
-	// Note: We don't filter by tenant here because Fosite interface is generic.
-	// We should validate tenant match in the Authorize Endpoint logic.
 	if err := s.DB.WithContext(ctx).First(&clientModel, "id = ?", id).Error; err != nil {
 		return nil, fosite.ErrNotFound
 	}
@@ -36,12 +36,48 @@ func (s *SQLStore) GetClient(ctx context.Context, id string) (fosite.Client, err
 		ResponseTypes: clientModel.ResponseTypes,
 		Scopes:        clientModel.Scopes,
 		Public:        clientModel.Public,
-		// We can attach TenantID to metadata if needed later, but Fosite DefaultClient doesn't support custom fields easily.
 	}, nil
 }
 
 func (s *SQLStore) ClientAssertionJWTValid(_ context.Context, _ string) error            { return nil }
 func (s *SQLStore) SetClientAssertionJWT(_ context.Context, _ string, _ time.Time) error { return nil }
+
+// --- Resource Owner Password Credentials Grant Storage (ROPC) ---
+
+func (s *SQLStore) Authenticate(ctx context.Context, name string, secret string) error {
+	var user models.User
+	if err := s.DB.WithContext(ctx).Where("email = ?", name).First(&user).Error; err != nil {
+		return fosite.ErrRequestUnauthorized
+	}
+
+	if !crypto.CheckPasswordHash(secret, user.PasswordHash) {
+		return fosite.ErrRequestUnauthorized
+	}
+
+	return nil
+}
+
+// --- RFC7523 Key Storage (JWT Bearer Grant) ---
+
+func (s *SQLStore) GetPublicKey(ctx context.Context, issuer string, subject string, keyId string) (*jose.JSONWebKey, error) {
+	return nil, fosite.ErrNotFound
+}
+
+func (s *SQLStore) GetPublicKeys(ctx context.Context, issuer string, subject string) (*jose.JSONWebKeySet, error) {
+	return &jose.JSONWebKeySet{}, fosite.ErrNotFound
+}
+
+func (s *SQLStore) GetPublicKeyScopes(ctx context.Context, issuer string, subject string, keyId string) ([]string, error) {
+	return []string{}, nil
+}
+
+func (s *SQLStore) IsJWTUsed(ctx context.Context, jti string) (bool, error) {
+	return false, nil
+}
+
+func (s *SQLStore) MarkJWTUsedForTime(ctx context.Context, jti string, exp time.Time) error {
+	return nil
+}
 
 // --- Storage Implementation (Common) ---
 
@@ -95,62 +131,90 @@ func (s *SQLStore) deleteSession(ctx context.Context, signature string) error {
 }
 
 // --- Access Token ---
+
 func (s *SQLStore) CreateAccessTokenSession(ctx context.Context, signature string, request fosite.Requester) error {
 	return s.createSession(ctx, signature, request, "access_token")
 }
+
 func (s *SQLStore) GetAccessTokenSession(ctx context.Context, signature string, session fosite.Session) (fosite.Requester, error) {
 	return s.getSession(ctx, signature, session, "access_token")
 }
+
 func (s *SQLStore) DeleteAccessTokenSession(ctx context.Context, signature string) error {
 	return s.deleteSession(ctx, signature)
 }
 
 // --- Refresh Token ---
+
 func (s *SQLStore) CreateRefreshTokenSession(ctx context.Context, signature string, request fosite.Requester) error {
 	return s.createSession(ctx, signature, request, "refresh_token")
 }
+
 func (s *SQLStore) GetRefreshTokenSession(ctx context.Context, signature string, session fosite.Session) (fosite.Requester, error) {
 	return s.getSession(ctx, signature, session, "refresh_token")
 }
+
 func (s *SQLStore) DeleteRefreshTokenSession(ctx context.Context, signature string) error {
 	return s.deleteSession(ctx, signature)
 }
 
 // --- Authorize Code ---
+
 func (s *SQLStore) CreateAuthorizeCodeSession(ctx context.Context, signature string, request fosite.Requester) error {
 	return s.createSession(ctx, signature, request, "authorize_code")
 }
+
 func (s *SQLStore) GetAuthorizeCodeSession(ctx context.Context, signature string, session fosite.Session) (fosite.Requester, error) {
 	return s.getSession(ctx, signature, session, "authorize_code")
 }
+
 func (s *SQLStore) InvalidateAuthorizeCodeSession(ctx context.Context, signature string) error {
 	return s.deleteSession(ctx, signature)
 }
 
 // --- Revocation ---
+
 func (s *SQLStore) RevokeRefreshToken(ctx context.Context, requestID string) error {
 	return s.RevokeAccessToken(ctx, requestID)
 }
+
+func (s *SQLStore) RevokeRefreshTokenMaybeGracePeriod(ctx context.Context, requestID string, signature string) error {
+	return s.RevokeRefreshToken(ctx, requestID)
+}
+
 func (s *SQLStore) RevokeAccessToken(ctx context.Context, requestID string) error {
 	return s.DB.WithContext(ctx).Where("request_id = ?", requestID).Delete(&models.OAuth2Session{}).Error
 }
 
-// --- PKCE & OpenID Connect ---
-func (s *SQLStore) CreatePKCESession(ctx context.Context, signature string, request fosite.Requester) error {
+func (s *SQLStore) RevokeAccessTokenMaybeGracePeriod(ctx context.Context, requestID string, signature string) error {
+	return s.RevokeAccessToken(ctx, requestID)
+}
+
+// --- PKCE (UPDATED METHOD NAMES) ---
+// Fosite expects CreatePKCERequestSession, GetPKCERequestSession, DeletePKCERequestSession
+
+func (s *SQLStore) CreatePKCERequestSession(ctx context.Context, signature string, request fosite.Requester) error {
 	return s.createSession(ctx, signature, request, "pkce")
 }
-func (s *SQLStore) GetPKCESession(ctx context.Context, signature string, session fosite.Session) (fosite.Requester, error) {
+
+func (s *SQLStore) GetPKCERequestSession(ctx context.Context, signature string, session fosite.Session) (fosite.Requester, error) {
 	return s.getSession(ctx, signature, session, "pkce")
 }
-func (s *SQLStore) DeletePKCESession(ctx context.Context, signature string) error {
+
+func (s *SQLStore) DeletePKCERequestSession(ctx context.Context, signature string) error {
 	return s.deleteSession(ctx, signature)
 }
+
+// --- OpenID Connect ---
+
 func (s *SQLStore) CreateOpenIDConnectSession(ctx context.Context, signature string, request fosite.Requester) error {
 	return s.createSession(ctx, signature, request, "oidc")
 }
+
 func (s *SQLStore) GetOpenIDConnectSession(ctx context.Context, signature string, request fosite.Requester) (fosite.Requester, error) {
 	return s.getSession(ctx, signature, request.GetSession(), "oidc")
 }
+
 func (s *SQLStore) DeleteOpenIDConnectSession(ctx context.Context, signature string) error {
 	return s.deleteSession(ctx, signature)
 }
